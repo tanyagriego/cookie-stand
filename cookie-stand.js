316b12bb63cb67eb1e2ba2050eda826dd0b816fb
tanyagriego <tanya.griego@gmail.com>
2017-08-16 20:47:33 -0700

    fixed methods. added getelemntbyid. created array for time

--- cookie-stand.js
+++ cookie-stand.js
@@ -5,9 +5,11 @@
   locationOne.max = 88;
   locationOne.average = 5.2;
     locationOne.CustPerHour = function () {
-      return Math.floor(Math.random () * 101);
+      var randomCustomer = Math.floor(Math.random () * (locationOne.max - locationOne.min)) + locationOne.min;
+      return randomCustomer
 };
 
+  // var simulatedCookies = locationOne.average * randomCustomer
 
 /*Location Two - PA*/
 var locationTwo = new Object ();
@@ -16,7 +18,8 @@ var locationTwo = new Object ();
   locationTwo.max = 24;
   locationTwo.average = 1.2;
     locationTwo.CustPerHour = function () {
-      return Math.floor(Math.random () * 101);
+      var randomCustomer = Math.floor(Math.random () * (locationTwo.max - locationTwo.min)) + locationTwo.min;
+      return randomCustomer
 };
 
 /*Location Three - WS*/
@@ -26,7 +29,8 @@ var locationThree = new Object ();
   locationThree.max = 38;
   locationThree.average = 1.9;
     locationThree.CustPerHour = function () {
-      return Math.floor(Math.random) () * 101);
+      var randomCustomer = Math.floor(Math.random () * (locationThree.max - locationThree.min)) + locationThree.min;
+      return randomCustomer
 };
 
 /*Location Four - Sellwood*/
@@ -36,8 +40,9 @@ var locationFour = new Object ();
   locationFour.max = 48;
   locationFour.average = 3.3;
     locationFour.CustPerHour = function () {
-      return Math.floor(Math.random) () * 101);
-    }
+      var randomCustomer = Math.floor(Math.random () * (locationFour.max - locationFour.min)) + locationFour.min;
+      return randomCustomer
+};
 
 /*Location Five - PD*/
 var locationFive = new Object ();
@@ -45,3 +50,24 @@ var locationFive = new Object ();
   locationFive.min = 3;
   locationFive.max = 24;
   locationFive.average = 2.6;
+    locationFive.CustPerHour = function () {
+      var randomCustomer = Math.floor(Math.random () * (locationFive.max - locationFive.min)) + locationFive.min;
+      return randomCustomer
+};
+
+var time = [
+  '10am',
+  '11am',
+  '12pm',
+  '1pm',
+  '2pm',
+  '3pm',
+  '4pm',
+  '5pm',
+];
+
+document.getElementById('pioneerSquare').innerHTML = locationOne.name;
+document.getElementById('portlandAirport').innerHTML = locationTwo.name;
+document.getElementById('washingtonSquare').innerHTML = locationThree.name;
+document.getElementById('sellwood').innerHTML = locationFour.name;
+document.getElementById('pearlDistrict').innerHTML = locationFive.name;
